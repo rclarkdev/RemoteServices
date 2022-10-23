e07aa690620ce37292d9d9489c2e145475515fb2
rclarkdev <rclarkdev1980@gmail.com>
2022-10-22 22:27:31 -0700

    first commit

---
+++ RemoteServices/ClientApp/src/app/contact-form/contact-form.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-contact-form',
+  templateUrl: './contact-form.component.html',
+  styleUrls: ['./contact-form.component.css'],
+})
+export class ContactFormComponent implements OnInit {
+
+  form: FormGroup;
+
+  ngOnInit(): void {
+    this.buildForm();
+  }
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  send(): void {
+    const { name, email, message } = this.form.value;
+    alert(`Name: ${name}, Email: ${email}, Message: ${message} `);
+  }
+
+  private buildForm(): void {
+    this.form = this.formBuilder.group({
+      name: this.formBuilder.control(null),
+      email: this.formBuilder.control(null),
+      message: this.formBuilder.control(null),
+    });
+  }
+}
